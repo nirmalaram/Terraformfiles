e2d666d118cb218cc750b3e01b231050ddceef62
nirmalaram <nirmalaram2008@gmail.com>
2024-05-22 16:05:34 -0700

    modified

--- count/main.tf
+++ count/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "countInstance" {
     ami = var.ami-id
     instance_type = var.instance-type
     key_name = var.key-name
-    count=3
+    count=length(var.environment)
     tags = {
         #Name="countinstance"
       #Name= "countinsance-${count.index}"
