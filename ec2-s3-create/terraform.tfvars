804c9e0f1a02672230692cdf7a8d7d7f3b2d6c83
nirmalaram <nirmalaram2008@gmail.com>
2024-05-25 00:29:04 -0700

    adding files

---
+++ ec2-s3-create/terraform.tfvars
@@ -0,0 +1,3 @@
+ami_id = "ami-07caf09b362be10b8"
+instancetype = "t2.micro"
+keyname = "NVirginiakey"
---
+++ ec2-s3-create/variables.tf
@@ -0,0 +1,17 @@
+variable "ami_id" {
+    description = "ami value"
+    type = string
+    default = ""
+  
+}
+variable "instancetype" {
+    description = "Type of the instance"
+    type = string
+    default = ""
+  
+}
+variable "keyname" {
+    type = string
+    default = ""
+  
+}
